35da4b05df97c3df3d5f75b275bebccbcb91deb3
iamapo <redenius.andre@googlemail.com>
2024-12-09 18:11:09 +0100

    Adding SerializedName

--- app/src/main/java/com/redred/tomorrowweatherapp/data/model/weather/WeatherResponse.kt
+++ app/src/main/java/com/redred/tomorrowweatherapp/data/model/weather/WeatherResponse.kt
@@ -1,19 +1,30 @@
 package com.redred.tomorrowweatherapp.data.model.weather
 
+import com.google.gson.annotations.SerializedName
+
 data class WeatherResponse(
+    @SerializedName("current_weather")
     val current_weather: CurrentWeather,
+    @SerializedName("hourly")
     val hourly: HourlyWeather
 )
 
 data class CurrentWeather(
+    @SerializedName("temperature")
     val temperature: Double,
+    @SerializedName("weathercode")
     val weathercode: Int,
+    @SerializedName("windspeed")
     val windspeed: Double,
+    @SerializedName("winddirection")
     val winddirection: Double,
+    @SerializedName("is_day")
     val is_day: Int
 )
 
 data class HourlyWeather(
+    @SerializedName("time")
     val time: List<String>,
+    @SerializedName("windspeed_10m")
     val windspeed_10m: List<Double>
 )
